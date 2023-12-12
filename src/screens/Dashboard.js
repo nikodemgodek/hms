eb41d3a90b985191b6b9208936ad8b33f55dcf6e
Nikodem Godek <nikodem.godek6@gmail.com>
2023-12-12 11:57:59 +0100

    Scratch dashboard

---
+++ src/screens/Dashboard.js
@@ -0,0 +1,10 @@
+import Navbar from "../components/dashboard/Navbar";
+
+export default function Dashboard() {
+    return(
+        <div>
+            <p>Dashboard page</p>
+            <Navbar />
+        </div>
+    )
+}
